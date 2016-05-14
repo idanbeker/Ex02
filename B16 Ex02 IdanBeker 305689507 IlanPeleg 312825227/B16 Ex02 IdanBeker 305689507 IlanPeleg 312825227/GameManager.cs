9108e058077fc0a720ee23c50b3f107bc2839d50
Ilan Peleg <ilan@gmail.com>
2016-05-14 19:26:53 +0300

    ilans changes 2 - more style to come..

--- B16 Ex02 IdanBeker 305689507 IlanPeleg 312825227/B16 Ex02 IdanBeker 305689507 IlanPeleg 312825227/GameManager.cs
+++ B16 Ex02 IdanBeker 305689507 IlanPeleg 312825227/B16 Ex02 IdanBeker 305689507 IlanPeleg 312825227/GameManager.cs
@@ -3,6 +3,7 @@ using System;
 
 class GameManager
 {
+    //TODO: organize the startgame func in functions
     private int m_PlayerOneScore;
     private int m_PlayerTwoScore;
     private Board m_GameBoard;
@@ -24,71 +25,105 @@ class GameManager
     public void StartGame()
     {
         int playerIndex = 1;
-        int currentPlayerInput;
-        bool isWon;
-
-        while (true) 
+        
+        while (true)
         {
             if (playerIndex == 1)
             {
-                currentPlayerInput = getPlayerInput(playerIndex);
-                if (currentPlayerInput != -1)//indicates whether the input was "Q" or not
-                {
-                    while (!m_GameBoard.EnterMove(currentPlayerInput, playerIndex, out isWon))//continue getting input while current input is invalid
-                    {
-                        System.Console.WriteLine("this column is full ");
-                        currentPlayerInput = getPlayerInput(playerIndex);
-                        if (currentPlayerInput == -1)//means that he enters "Q"
-                        {
-                            break;
-                        }
-                    }
-                    if (isWon)
-                    {
-                        m_PlayerOneScore++;
-                        System.Console.WriteLine("player one won! ");
-                        System.Console.WriteLine("player one's score is: " + m_PlayerOneScore + " palyer two's score is: " + m_PlayerTwoScore);
-                        newGame();
-                    }
-                }
+                manageTurn(1);
                 playerIndex++;
             }
             else
             {
-                currentPlayerInput = getPlayerInput(playerIndex);
-                if (currentPlayerInput != -1)//indicates whether the input was "Q" or not
+                if (m_GameMode == eGameMode.MultyPlayer)
                 {
-                    while (!m_GameBoard.EnterMove(currentPlayerInput, playerIndex, out isWon))//continue getting input while current input is invalid
-                    {
-                        System.Console.WriteLine("this column is full, please enter another column ");
-                        currentPlayerInput = getPlayerInput(playerIndex);
-                        if (currentPlayerInput == -1)//means that he enters "Q"
-                        {
-                            break;
-                        }
-                    }
-                    if (isWon)
-                    {
-                        m_PlayerTwoScore++;
-                        System.Console.WriteLine("player two won! ");
-                        System.Console.WriteLine("player one's score is: " + m_PlayerOneScore + " palyer two's score is: " + m_PlayerTwoScore);
-                        newGame();
-                    }
+                    manageTurn(2);
+                }
+                else //the game is single-player. the computer chooses randomly empty column
+                {
+                    managComputersTurn();
                 }
                 playerIndex--;
             }
             System.Console.WriteLine(m_GameBoard.ToString());
             //check if the board is full-meaning we have a tie
-           if (m_GameBoard.BoardIsFull())
+            handleTieCase();
+        }
+
+    }
+
+    private void handleTieCase()
+    {
+        if (m_GameBoard.BoardIsFull())
+        {
+            System.Console.WriteLine("we have a tie! ");
+            System.Console.WriteLine("player one's score is: " + m_PlayerOneScore + " palyer two's score is: " + m_PlayerTwoScore);
+            newGame();
+        }
+    }
+
+    private void managComputersTurn()
+    {
+        bool isWon;
+        int playerIndex = 2;
+        Random computersChoiceRnd = new Random();
+    
+        //chooses a random number between 0-(m_BoardLength-1)
+        int computersChoiceCol = computersChoiceRnd.Next(m_BoardLength - 1);
+        System.Console.WriteLine("the computer's move: ");
+        while (!m_GameBoard.EnterMove(computersChoiceCol, playerIndex, out isWon))//continue choosing random number while current one is invalid
+        {
+            computersChoiceCol = computersChoiceRnd.Next(m_BoardLength - 1);
+        }
+        if (isWon)
+        {
+            m_PlayerTwoScore++;
+            System.Console.WriteLine("the computer has won! ");
+            System.Console.WriteLine("player one's score is: " + m_PlayerOneScore + " computer's score is: " + m_PlayerTwoScore);
+            newGame();
+        }
+
+    }
+    private void manageTurn(int i_playerIndex)
+    {
+        //  int playerIndex = 1;
+        int currentPlayerInput;
+        bool isWon;
+        currentPlayerInput = getPlayerInput(i_playerIndex);
+        if (currentPlayerInput != -1)//indicates whether the input was "Q" or not
+        {
+            //continue getting input while current input is invalid
+            while (!m_GameBoard.EnterMove(currentPlayerInput, i_playerIndex, out isWon))//isWon recieves if the last move was a winning move
             {
-                System.Console.WriteLine("we have a tie! ");
-                System.Console.WriteLine("player one's score is: " + m_PlayerOneScore + " palyer two's score is: " + m_PlayerTwoScore);
-                newGame();
+                System.Console.WriteLine("this column is full ");
+                currentPlayerInput = getPlayerInput(i_playerIndex);
+                if (currentPlayerInput == -1)//means that he enters "Q"
+                {
+                    break;
+                }
             }
 
+            //we have a move- lets check if its a winning move!
+            if (isWon)
+            {
+                if (i_playerIndex == 1)
+                {
+                    m_PlayerOneScore++;
+                    System.Console.WriteLine("player one won! ");
+                    System.Console.WriteLine("player one's score is: " + m_PlayerOneScore + " palyer two's score is: " + m_PlayerTwoScore);
+                    newGame();
+                }
+                else
+                {
+                    m_PlayerTwoScore++;
+                    System.Console.WriteLine("player two won! ");
+                    System.Console.WriteLine("player one's score is: " + m_PlayerOneScore + " palyer two's score is: " + m_PlayerTwoScore);
+                    newGame();
+                }
+            }
         }
-       
     }
+
 
 
     //ask the user whether he wants to keep playing.
@@ -96,6 +131,7 @@ class GameManager
     //if yes- initialize the board
     private void newGame()
     {
+        System.Console.WriteLine(m_GameBoard.ToString());//show the last-updated board
         bool wantsAnotherGame = false;
         string userInput;
         do
@@ -116,9 +152,9 @@ class GameManager
             Environment.Exit(0);
         }
         this.m_GameBoard = new Board(m_BoardHeight, m_BoardLength);
-      }
+    }
 
-   
+
 
     private int getPlayerInput(int i_PlayerIndex)
     {
@@ -126,12 +162,12 @@ class GameManager
         bool validParse = false;
         bool validNumber = false;
 
-        int parsedInput=-1;
+        int parsedInput = -1;
         do
         {
-            System.Console.Write("Enter the number of the cell you want to enter a coin"+ Environment.NewLine);
+            System.Console.Write("Enter the number of the cell you want to enter a coin" + Environment.NewLine);
             string userInput = System.Console.ReadLine();
-            isQEntered=QEntered(userInput,i_PlayerIndex); //checks if Q was entered and handled the case in which it was
+            isQEntered = QEntered(userInput, i_PlayerIndex); //checks if Q was entered and handled the case in which it was
             if (isQEntered)
             {
                 newGame();
@@ -144,16 +180,16 @@ class GameManager
             }
             else
             {
-                System.Console.Write("you should enter only numbers"+ Environment.NewLine);
+                System.Console.Write("you should enter only numbers" + Environment.NewLine);
             }
         } while (!validParse || !validNumber);
 
         return parsedInput;
     }
 
-    private bool QEntered (string i_Input, int i_PlayerIndex)
+    private bool QEntered(string i_Input, int i_PlayerIndex)
     {
-        bool isQEntered= (i_Input.Equals("Q"));
+        bool isQEntered = (i_Input.Equals("Q"));
         if (isQEntered)
         {
             if (i_PlayerIndex == 1)
@@ -175,15 +211,15 @@ class GameManager
     private bool isValidColumnInput(int i_Column)
     {
         bool isValidColumnInput = false;
-        if (i_Column<m_BoardLength && i_Column >= 0)
+        if (i_Column < m_BoardLength && i_Column >= 0)
         {
             isValidColumnInput = true;
         }
         else
         {
             isValidColumnInput = false;
-            System.Console.Write("your number is not in the right range"+ Environment.NewLine);
-          }
+            System.Console.Write("your number is not in the right range" + Environment.NewLine);
+        }
 
         return isValidColumnInput;
     }
